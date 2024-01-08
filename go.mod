71cdffc46b3b2796e5cc34cc10bfd853b38d8511
chandan <chandanky.23@gmail.com>
2024-01-08 11:29:11 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chandanky23/go-logger
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,15 @@
+package logger
+
+import "log"
+
+func LogInfo(message string) {
+	log.Printf("INFO - %v", message)
+}
+
+func LogError(message string) {
+	log.Printf("INFO - %v", message)
+}
+
+func LogWarning(message string) {
+	log.Printf("INFO - %v", message)
+}
